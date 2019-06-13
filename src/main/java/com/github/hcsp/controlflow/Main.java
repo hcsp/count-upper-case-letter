840b77c239a1fa74ba1bf11fd8bdffe29c7b84db
zyw <40329182+zhangyongwang4444@users.noreply.github.com>
2019-06-13 23:54:59 +0800

    统计字符串中大写字母的次数 (#5)
    统计字符串中大写字母的次数 (#5)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,9 +10,18 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int num = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch >= 'A' && ch <= 'Z') {
+                num++;
+            }
+        }
+        return num;
+    }
 
     public static void main(String[] args) {
+
         countUpperCaseLetters("AaBbCc1234ABC");
     }
 }
