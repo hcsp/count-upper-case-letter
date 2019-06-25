b669ee7f6e8c939383c22fbe4cf0a48208fd13f2
squall1744 <squall1744@163.com>
2019-06-25 22:14:41 +0800

    count upper case letter

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -19,6 +19,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(countUpperCaseLetters("AaBbCc1234ABC"));
+        countUpperCaseLetters("AaBbCc1234ABC");
     }
 }
