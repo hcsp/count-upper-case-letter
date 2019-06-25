041d3ffbf483253a092afc2f1b5516b14f4bc338
catsme <845058547@qq.com>
2019-06-25 10:42:26 +0800

    实现了找出一串字符中所有的大写字符，并返回次数的功能。

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,8 +1,6 @@
 package com.github.hcsp.controlflow;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     /**
@@ -15,7 +13,7 @@ public class Main {
      */
     public static int countUpperCaseLetters(String str) {
         int count=0;//定义一个计数器count
-           ArrayList<String> arr=new ArrayList<String>();//新建一个arr列表，用于存放字符串中的大写字符。
+           ArrayList<String> arr= new ArrayList<>();//新建一个arr列表，用于存放字符串中的大写字符。
            for (int i =0; i <str.length() ; i++) {//对字符串进行遍历
             if('A'<=str.toCharArray()[i]&&str.toCharArray()[i]<='Z'){//判断字符是否为大写字符
                arr.add(String.valueOf(str.charAt(i)));//将大写字符存入arr列表中。
