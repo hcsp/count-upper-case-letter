54708483b0209a151efcf60d8509b149897378a7
Bo Zhang <bo@gradle.com>
2019-06-27 10:32:24 +0800

    Add magic number test

--- src/test/java/com/github/hcsp/controlflow/MainTest.java
+++ src/test/java/com/github/hcsp/controlflow/MainTest.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.controlflow;
 
+import com.github.hcsp.test.helper.ProjectSourceFileReader;
 import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,5 +19,12 @@ public class MainTest {
         Assertions.assertEquals(
                 (int) str.chars().filter(Character::isUpperCase).count(),
                 Main.countUpperCaseLetters(str));
+    }
+
+    @Test
+    public void noMagicNumbers() {
+        String sourceCode = ProjectSourceFileReader.readAsString(Solution.class);
+        Assertions.assertTrue(
+                Stream.of("65", "90", "0x41", "0x5a").noneMatch(sourceCode::contains));
     }
 }
