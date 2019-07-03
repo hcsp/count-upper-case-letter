7cb05debef8944b3bf2aeb4f1f9dc0ea023f1f47
༺༽༾ཊ 船长 ཏ༿༼༻ <captaininphw@gmail.com>
2019-07-03 21:56:12 +0800

    Update Main.java (#25)
    Update Main.java (#25)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
     /**
      * 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
@@ -9,7 +12,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int counter = 0;
+        char[] chArr = str.toCharArray();
+        for (char ch : chArr) {
+            if (Character.isUpperCase(ch)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
