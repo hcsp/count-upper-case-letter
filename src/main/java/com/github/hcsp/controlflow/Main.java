3250117dc67a0ff2e5c8ffec610abc2daf387509
Bigbig Zheng <39644918+zhengbigbig@users.noreply.github.com>
2019-07-23 15:04:50 +0800

    foreach isUpperCase toCharArray (#33)
    foreach isUpperCase toCharArray (#33)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     /**
      * 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
@@ -9,7 +12,14 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int num = 0;
+        for (char cc : str.toCharArray()) {
+            if (Character.isUpperCase(cc)) num += 1;
+        }
+        System.out.println(num);
+        return num;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
