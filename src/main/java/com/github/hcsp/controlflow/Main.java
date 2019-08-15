096afe8322900e383577e36fec8d80ee48cee79a
25127 <jie.wang13@hand-china.com>
2019-08-15 23:33:17 +0800

    统计大写字母。注意char其实就是个数字

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        char[] arg1 = str.toCharArray();
+        int i = 0;
+        for (char item : arg1) {
+            if (item >= 'A' && item <= 'Z'){
+                ++i;
+            }
+        }
+        return i;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
