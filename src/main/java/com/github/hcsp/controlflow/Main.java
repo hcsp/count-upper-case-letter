9cbe2347266b99450888d35b7ad4dcad2a415d25
OrangeC <38210165+903875816@users.noreply.github.com>
2019-09-15 18:25:18 +0800

    统计字符串中大写英文字母出现的次数 (#58)
    统计字符串中大写英文字母出现的次数 (#58)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.controlflow;
 
-public class Main {
+public class Main<count> {
     /**
      * 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
      *
@@ -9,9 +9,29 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
-
+    public static int countUpperCaseLetters(String str) {
+        //char[] stringArr = str.toCharArray();
+        int count=0;
+        //for(int i=0;i<stringArr.length;i++) {
+        //    //if(stringArr[i]>=5*13&&stringArr[i]<=6*15){
+        //    //    count++;
+        //    if (stringArr[i] <= 'Z' && stringArr[i] >= 'A') {
+        //        count++;
+        //    } else {
+        //        continue;
+        //    }
+        //}
+        String Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(Upper.indexOf(c)!=-1){
+                count++;
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
-        countUpperCaseLetters("AaBbCc1234ABC");
+        int s=countUpperCaseLetters("AaBbCc1234ABC");
+        System.out.println(s);
     }
 }
