157efc718bd65640e7d06eda9d1faf643a2cca21
qiaomengnan16 <285921957@qq.com>
2019-10-02 07:49:18 +0800

    equals up (#69)
    equals up (#69)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        String [] strings = str.split("");
+        int count = 0;
+        for(String temp : strings){
+            if(temp.equals(temp.toUpperCase()) && temp.matches("[a-zA-Z]+") ) {
+                count++;
+            }
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
