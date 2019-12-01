0a9c82964a79d5badedbddac5774fe9b54c2503c
Leon <z429882801@qq.com>
2019-12-01 17:58:07 +0800

    Java的控制流练习：统计字符串中大写英文字母出现的次数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println((int)str.charAt(i));
+            if((int)str.charAt(i)>=65 && (int)str.charAt(i) <=90)
+            sum++;
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
