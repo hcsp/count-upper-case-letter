c8eb4da543c0a91bf8e5183323d0e1a8f2d54f5a
Storm4542 <417002107@qq.com>
2019-12-03 15:32:55 +0800

    返回大写字母数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int count = 0;
+        char[] temp = str.toCharArray();
+        for (Character word : temp) {
+            if (Character.isUpperCase(word)) {
+                count += 1;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
