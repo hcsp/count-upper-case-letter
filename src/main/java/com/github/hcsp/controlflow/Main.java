924a1cfa02283909c2da1fe6e212614625e45ffe
linsinan1995 <47880367+linsinan1995@users.noreply.github.com>
2019-12-14 22:38:32 +0100

    count uppercase letters (#103)
    count uppercase letters (#103)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,13 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int cnt = 0;
+        for (int idx=0; idx<str.length();++idx){
+            if (Character.isUpperCase(str.charAt(idx))) cnt++;
+        }
+        return cnt;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
