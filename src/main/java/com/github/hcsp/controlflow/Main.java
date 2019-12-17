42e652e4ae36ae93228dd9d629291688188a4fce
tiantian <1500846601@qq.com>
2019-12-17 16:41:17 +0800

    统计字符串中大写英文字母出现的次数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int b = 0;
+        for (int i = 0; i < str.length(); i++) {
+            char a = str.charAt(i);
+            if(a >= 65 && a <= 90) {
+                b++;
+            }
+        }
+        return b;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
