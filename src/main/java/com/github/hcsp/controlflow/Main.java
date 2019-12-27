954bceefeb92bf30f1370d181833020d890f23c6
oylx <oylx2008@163.com>
2019-12-27 17:05:10 +0800

    Character.isUpperCase判断大写字母 (#111)
    Character.isUpperCase判断大写字母 (#111)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,9 +9,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int count = 0;
+        for (char ele : str.toCharArray()) {
+            count += (ele >='A' && ele <= 'Z') ? 1 : 0; // Character.isUpperCase(ele)
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
-        countUpperCaseLetters("AaBbCc1234ABC");
+
+        int x = countUpperCaseLetters("AaBbCc1234ABC");
+        System.out.println(x);
     }
 }
