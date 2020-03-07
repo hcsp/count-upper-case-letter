eee09a4e3357e09f42266a732152283ddca49f73
jslyq <jsfjlyq@gmail.com>
2020-03-07 16:28:12 -0500

    大写字母计数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.controlflow;
 
-public class Main {
+public class
+Main {
     /**
      * 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
      *
@@ -9,7 +10,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int UpperCase = 0;
+        for (int i = 0; i <str.length(); i++){
+            char ch = str.charAt(i);
+            if((ch >= 'A')&& (ch <= 'Z')){
+                UpperCase++;
+            }
+        }
+        System.out.println("大写字母个数为："+UpperCase);
+        return UpperCase;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
