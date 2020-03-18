b3d25d57fecd12ef8a2ac9356d22aa1fffafcac3
charles-geng <59286279+charles-geng@users.noreply.github.com>
2020-03-18 11:00:39 +0800

    统计字符串中大写英文字母出现的次数 (#161)
    统计字符串中大写英文字母出现的次数 (#161)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+
+
 public class Main {
     /**
      * 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
@@ -9,7 +11,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int a = 0;   // 统计次数的一个基准
+        for (int i = 0; i < str.length(); i++) {//循环字符串
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {//判断字符串是不是大写英文字母
+                a++;//是大写英文字母基准数据加一
+            }
+        }
+        return a;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
