7016bb4d8841b88b283c3001e31d25b833faaa5c
yujie-zhang <907420318@qq.com>
2020-04-28 15:56:24 +0800

    统计字符串中大写字母出现的次数 (#176)
    统计字符串中大写字母出现的次数 (#176)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,15 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int charCount = 0;
+        for (char temp : str.toCharArray()) {
+            if ('A' <= temp && temp <= 'Z') {
+                charCount++;
+            }
+        }
+        return charCount ;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
