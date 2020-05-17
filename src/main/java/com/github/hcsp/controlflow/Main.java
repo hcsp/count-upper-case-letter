f9c1dcc7aff66afb2572a3283c74494f2306bdb8
hanchenhao <hanchenhao@vip.qq.com>
2020-05-17 19:44:53 +0800

    upper count (#182)
    upper count (#182)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,9 +9,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+
+        int count = 0;
+        for (char e: str.toCharArray()) {
+            if (e >= 'A' && e <= 'Z') count += 1;
+        }
+        return count;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
+        System.out.println( countUpperCaseLetters("AaBbCc1234ABC"));
     }
 }
