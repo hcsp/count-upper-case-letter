7853f7c0f38881053fe7edf2a74cdcde51a8004e
catmmao <catmmao@gmail.com>
2020-10-08 14:54:30 +0800

    完善代码

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,20 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int strLen = str.length();
+        int count = 0;
+        char element;
+
+        for (int i = 0; i < strLen; i++) {
+            element = str.charAt(i);
+            if ('A' <= element && element <= 'Z') {
+                count++;
+            }
+        }
+
+        return count;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
