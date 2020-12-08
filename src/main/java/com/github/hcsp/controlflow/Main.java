541c104db6101c0a30dfd21beff7f51047f80a92
XuKaiQuan <964262001@qq.com>
2020-12-08 19:27:25 +0800

    ：统计字符串中大写英文字母出现的次数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static int countUpperCaseLetters(String str) {
         int count = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i)>='A' && str.charAt(i)<='Z'){
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
                 count++;
             }
         }
