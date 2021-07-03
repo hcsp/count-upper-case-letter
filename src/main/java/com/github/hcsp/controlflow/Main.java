04a175f9bd7a61049d8d14759c7516365d1dad24
skyszhang <1296003529@qq.com>
2021-07-03 16:25:02 +0800

    count upper case letter

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,16 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int sum = 0;
+        for (char i : str.toCharArray()
+        ) {
+            if (Character.isUpperCase(i)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
