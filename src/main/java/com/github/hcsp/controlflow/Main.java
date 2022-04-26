fedfe81c146ae24681139732871551a5084119c4
NAyato <qq745847188@gmail.com>
2022-04-26 20:44:02 +0800

    11

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.controlflow;
 
+
 public class Main {
+
     /**
      * 统计一个给定的字符串中，大写英文字母（A,B,C,...,Z）出现的次数。
      *
@@ -9,7 +11,17 @@ public class Main {
      * @param str 给定的字符串
      * @return 字符串中大写英文字母出现的次数
      */
-    public static int countUpperCaseLetters(String str) {}
+    public static int countUpperCaseLetters(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+                count++;
+            }
+            System.out.println(count);
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         countUpperCaseLetters("AaBbCc1234ABC");
